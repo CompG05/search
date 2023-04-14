56e73ce2ec1595f554483183e10163ee53091370
PabloRoss7 <pablorossi2001@gmail.com>
2023-04-14 10:34:39 -0300

    Implement a* algorithm
    Co-authored-by: Joel D'Autilio <Jota98daut@users.noreply.github.com>

---
+++ algorithms/informed/astar.py
@@ -0,0 +1,12 @@
+from typing import Callable
+
+from algorithms.informed.best_first_search import BestFirstSearch
+from algorithms.search_algorithm import Node
+
+
+class Astar(BestFirstSearch):
+    def __init__(self, heuristic: Callable[[Node], float]):
+        super().__init__(lambda node: node.path_cost + heuristic(node))
+
+    def set_heuristic(self, new_heuristic: Callable[[Node], float]):
+        self.heuristic = (lambda node: node.path_cost + new_heuristic(node))
