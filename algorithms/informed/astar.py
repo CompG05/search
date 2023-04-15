790df2b73da30613eedefba9d36516b33c474f5c
Joel D'Autilio <joeldautilio@gmail.com>
2023-04-15 02:19:30 -0300

    Slight rename

--- algorithms/informed/astar.py
+++ algorithms/informed/astar.py
@@ -4,7 +4,7 @@ from algorithms.informed.best_first_search import BestFirstSearch
 from algorithms.search_algorithm import Node
 
 
-class Astar(BestFirstSearch):
+class AStar(BestFirstSearch):
     def __init__(self, heuristic: Callable[[Node], float]):
         super().__init__(lambda node: node.path_cost + heuristic(node))
 
