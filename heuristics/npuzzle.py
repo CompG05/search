6132eefeb07e104d2a7e12a1bd7c09e8f1d54da4
PabloRoss7 <pablorossi2001@gmail.com>
2023-04-17 20:53:02 -0300

    Implement manhattan distance heuristic for n-puzzle
    Co-authored-by: Joel D'Autilio <Jota98daut@users.noreply.github.com>

--- heuristics/npuzzle.py
+++ heuristics/npuzzle.py
@@ -7,6 +7,8 @@ class NPuzzleHeuristic:
     def create(self, heuristic: str) -> Callable[[Node], float]:
         if heuristic.lower() == "wrong_tiles":
             return self.wrong_tiles
+        if heuristic.lower() == "manhattan_distance":
+            return self.manhattan_distance
         else:
             raise ValueError("Heuristic not found")
 
@@ -14,3 +16,19 @@ class NPuzzleHeuristic:
     def wrong_tiles(node: Node) -> float:
         state = node.state
         return sum(1 for i, j in zip(state.data, range(0, state.size)) if i != j)
+
+    @staticmethod
+    def manhattan_distance(node: Node) -> float:
+        state = node.state
+        dim = state.dimension
+        manhattan_distance = 0
+        for i in range(0, len(state)):  #i indicates the position where piece i should be in the goal state
+            i_goal_row = i / dim
+            i_goal_col = i % dim
+
+            i_current_pos = state.index(i)
+            i_current_row = i_current_pos / dim
+            i_current_col = i_current_pos / dim
+
+            manhattan_distance += abs(i_goal_row - i_current_row) + abs(i_goal_col - i_current_col)
+        return manhattan_distance
