85c1b7973cc3b78034fb9ebc5850fb80824804b1
Joel D'Autilio <joeldautilio@gmail.com>
2023-04-28 00:42:05 -0300

    Write csv methods

--- algorithms/instrumented_solver.py
+++ algorithms/instrumented_solver.py
@@ -1,5 +1,6 @@
 import sys
 import time
+from typing import Optional
 
 from algorithms.solver import Solution, Solver
 from algorithms.search_algorithm import Node
@@ -7,11 +8,12 @@ from problems.problem import InstrumentedProblem, InvertibleProblem, Instrumente
 
 
 class InstrumentedSolution(Solution):
-    def __init__(self, node: Node | None, algorithm_name: str, dtime, problem: InstrumentedProblem):
-        super().__init__(node, algorithm_name)
+    def __init__(self, node: Node | None, algorithm_name: str, heuristic_name: str, dtime, problem: InstrumentedProblem):
+        super().__init__(node, algorithm_name, heuristic_name)
         self.problem = problem
         self.node_size = sys.getsizeof(node)
-        self.time = dtime
+        self.time = dtime * 1000
+        self.memory = self.problem.max_nodes_in_frontier * self.node_size
 
     def __str__(self):
         return super().__str__() + f"""
@@ -19,13 +21,20 @@ time: %.2f ms
 branching factor: %.4f
 generated nodes: {self.problem.nodes}
 max nodes in frontier: {self.problem.max_nodes_in_frontier}
-max memory usage: {self.problem.max_nodes_in_frontier * self.node_size} bytes"""\
-            % (self.time * 1000, self.problem.branching_factor)
+max memory usage: {self.memory} bytes"""\
+            % (self.time, self.problem.branching_factor)
+
+    @classmethod
+    def csv_header(cls):
+        return super().csv_header() + ",time (ms),nodes,memory (bytes)"
+
+    def to_csv(self):
+        return super().to_csv() + f",{self.time},{self.problem.nodes},{self.memory}"
 
 
 class InstrumentedSolver(Solver):
-    def __init__(self, problem: str, initial_state, algorithm: str, heuristic: str, *args):
-        super().__init__(problem, initial_state, algorithm, heuristic, *args)
+    def __init__(self, problem: str, initial_state, algorithm: str, heuristic: Optional[str]):
+        super().__init__(problem, initial_state, algorithm, heuristic)
         if isinstance(self.problem, InvertibleProblem):
             self.problem = InstrumentedInvertibleProblem(self.problem)
         else:
@@ -37,4 +46,4 @@ class InstrumentedSolver(Solver):
         node = self.algorithm.search(self.problem)
         after = time.time()
 
-        return InstrumentedSolution(node, self.algorithm.__class__.__name__, after - before, self.problem)
+        return InstrumentedSolution(node, self.algorithm.__class__.__name__, self.heuristic.__name__, after - before, self.problem)
