0fa08ee6eaee465e69a8a89af3bf45efdf964db5
Joel D'Autilio <joeldautilio@gmail.com>
2023-05-04 17:45:47 -0300

    Program to continuously generate big n-puzzle states
    Co-authored-by: PabloRoss7 <pablorossi2001@gmail.com>

---
+++ generators/generate_hard_states.py
@@ -0,0 +1,48 @@
+import signal
+import os
+import sys
+
+from algorithms.instrumented_solver import InstrumentedSolution
+from algorithms.solver import Solver
+from generators import generate_npuzzle_state
+from constants import *
+
+TIME_LIMIT = 90
+
+
+def handle_timeout(signum, frame):
+    raise TimeoutError
+
+
+def nonstop_states(filename, dimension: int):
+    if dimension < 4:
+        print("WARNING: This program is meant to generate states for 15-Puzzle or bigger")
+
+    signal.signal(signal.SIGALRM, handle_timeout)
+
+    if not os.path.exists(filename):
+        with open(filename, "w") as f:
+            f.write(InstrumentedSolution.csv_header())
+
+    while True:
+        state = generate_npuzzle_state(dimension)
+        solver = Solver(NPUZZLE, state, A_STAR, MANHATTAN_DISTANCE)
+
+        signal.alarm(TIME_LIMIT)
+        try:
+            _ = solver.solve()
+            with open(filename, "a") as f:
+                f.write(",".join(str(x) for x in state) + "\n")
+        except TimeoutError:
+            pass
+
+
+def main():
+    args = sys.argv[1:]
+    if len(args) != 2:
+        print("Usage: %s <output_file> <dimension>" % sys.argv[0])
+    nonstop_states(args[0], int(args[1]))
+
+
+if __name__ == "__main__":
+    main()
