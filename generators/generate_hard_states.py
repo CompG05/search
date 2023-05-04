61ca1b838d1e35ae2275d46b047cb181ac7e1032
PabloRoss7 <pablorossi2001@gmail.com>
2023-05-04 18:14:18 -0300

    Remove header to generate_hard_states.py
    Co-authored-by: Joel D'Autilio <Jota98daut@users.noreply.github.com>

--- generators/generate_hard_states.py
+++ generators/generate_hard_states.py
@@ -20,13 +20,9 @@ def nonstop_states(filename, dimension: int):
 
     signal.signal(signal.SIGALRM, handle_timeout)
 
-    if not os.path.exists(filename):
-        with open(filename, "w") as f:
-            f.write(InstrumentedSolution.csv_header())
-
     while True:
         state = generate_npuzzle_state(dimension)
-        solver = Solver(NPUZZLE, state, A_STAR, MANHATTAN_DISTANCE)
+        solver = Solver(NPUZZLE, state, A_STAR, WRONG_ROW_COL)
 
         signal.alarm(TIME_LIMIT)
         try:
