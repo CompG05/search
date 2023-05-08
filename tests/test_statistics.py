1e5172e11b706065a9bc5a30c6042b3519064ff4
Joel D'Autilio <joeldautilio@gmail.com>
2023-05-08 20:00:04 -0300

    Remove inconsistent test
    Co-authored-by: PabloRoss7 <pablorossi2001@gmail.com>

--- tests/test_statistics.py
+++ tests/test_statistics.py
@@ -8,28 +8,34 @@ from problems.problem import InstrumentedProblem
 from constants import *
 
 
-def test_memory_profiling():
-    state = (
-        3, 1, 2,
-        4, 0, 5,
-        6, 7, 8,
-    )
-
-    bfs = BreadthFirstSearch()
-    problem = NPuzzleProblem(state)
-
-    tm.start()
-
-    solution_node = bfs.search(problem)
-
-    tm.take_snapshot()
-
-    peak_memory_in_bytes = tm.get_traced_memory()[1]
-    max_nodes_in_frontier_estimate = peak_memory_in_bytes / sys.getsizeof(NPuzzleState)
-
-    tm.stop()
-
-    assert 12 <= max_nodes_in_frontier_estimate <= 31
+# def test_memory_profiling():
+#     """For an initial state with a known search tree
+#     check that the peak memory usage is about the expected.
+#     """
+#     state = (
+#         3, 1, 2,
+#         4, 0, 5,
+#         6, 7, 8,
+#     )
+#
+#     bfs = BreadthFirstSearch()
+#     problem = NPuzzleProblem(state)
+#
+#     tm.start()
+#
+#     solution_node = bfs.search(problem)
+#
+#     tm.take_snapshot()
+#
+#     peak_memory_in_bytes = tm.get_traced_memory()[1]
+#     max_nodes_in_frontier_estimate = peak_memory_in_bytes / sys.getsizeof(NPuzzleState)
+#
+#     tm.stop()
+#
+#     # Using breadth-first search, the solution is found at depth 2
+#     # Best case: the solution is found before the next level is expanded (frontier nodes: 12)
+#     # Worst case: it's found after the next level was completly expanded (frontier nodes: 31)
+#     assert 12 <= max_nodes_in_frontier_estimate <= 31
 
 
 def test_nodes_count():
