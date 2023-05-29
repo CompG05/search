cff83b73bad36bad2350f837165e7a3a7c21c56e
Joel D'Autilio <joeldautilio@gmail.com>
2023-05-28 21:33:41 -0300

    Add tests for hill climbing
    Co-authored-by: Pablo Rossi <pablorossi2001@gmail.com>

--- tests/test_hill_climbing.py
+++ tests/test_hill_climbing.py
@@ -1,39 +1,48 @@
 import pytest
 
 from algorithms.local.hill_climbing import HillClimbingSideMovements, HillClimbing, RandomRestartHillClimbing
-from constants import NQUEENS, INVERSE_N_CONFLICTS
+from constants import *
 from generators import generate_nqueens_state
+from heuristics.knapsack import KnapsackHeuristic
 from heuristics.nqueens import NQueensHeuristic
+from problems.knapsack import KnapsackProblem, KnapsackState
 from problems.nqueens import NQueensState, NQueensProblem
 
 p = NQueensProblem(generate_nqueens_state(8))
 h = NQueensHeuristic().create(INVERSE_N_CONFLICTS)
-
-rating_config = [
-    (HillClimbing(h), 0.14),
-    (HillClimbingSideMovements(h, 100), 0.94),
-]
+h2 = KnapsackHeuristic().create(ACCUM_VALUE)
 
 
-@pytest.mark.parametrize("algorithm, solution_rate", rating_config)
-def test_sideways_hill_climbing_nqueens(algorithm, solution_rate):
-    i = 1000
-
-    states = [NQueensState(generate_nqueens_state(8)) for _ in range(i)]
-
-    solved = 0
-    for state in states:
-        p.initial_state = state
-        result = algorithm.search(p)
-        solved += result.state.is_goal()
-
-    actual_solution_rate = solved / i
-    print("Solved:", solved)
-
-    assert solution_rate - 0.05 <= actual_solution_rate <= solution_rate + 0.05
+hc_algorithms = [HillClimbing(h), HillClimbingSideMovements(h, 100), RandomRestartHillClimbing(h, exhaustive=True)]
 
 
-def test_random_restart_nqueens():
-    algorithm = RandomRestartHillClimbing(NQUEENS, h)
+def test_hill_climbing_nqueens():
+    i = 100
+
+    state_factory = p.state_factory
+    states = [state_factory.random() for _ in range(i)]
+
+    for hc in hc_algorithms:
+        solved = 0
+        for state in states:
+            p.initial_state = state
+            result = hc.search(p)
+            solved += result.state.is_goal()
+
+        actual_solution_rate = solved / i
+        print(f"\n{hc.__class__.__name__} solution rate: {actual_solution_rate}")
+
+
+def test_random_restart_exhaustive_nqueens():
+    algorithm = RandomRestartHillClimbing(h)
     solution = algorithm.search(p)
     assert solution.state.is_goal()
+
+def test_random_restart_non_exhaustive_knapsack():
+    weights = [5, 10, 30, 15, 40, 25, 65]
+    values = [1, 2, 7, 3, 9, 6, 10]
+    cap = 75
+    algorithm = RandomRestartHillClimbing(h2, False, 10)
+    problem = KnapsackProblem(set(), weights, values, cap)
+    print("\n", algorithm.search(problem).state)
+
